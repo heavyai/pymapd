aebd8ad3926904adfbc636b06551b23f0d3ec0f6
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-20 11:51:01 -0500

    PKG: remove extraneous init

--- scripts/generate_bindings.sh
+++ scripts/generate_bindings.sh
@@ -4,3 +4,4 @@ thrift -gen py -out ${1-.} ${MAPD_HOME}/mapd.thrift
 mv ${1-.}/mapd/ttypes.py ${1-.}/mapd/ttypes-backup.py
 echo "Fixing recursive structs"
 python ${MAPD_HOME}/SampleCode/fix_recursive_structs.py ${1-.}/mapd/ttypes-backup.py ${1-.}/mapd/ttypes.py
+rm ${1-.}/__init__.py
