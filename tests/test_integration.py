eebfbc5e7860678c68640db4d94c4b2103f3cdbf
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-21 10:19:54 -0500

    CI: Added Travis

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,7 +2,19 @@
 Tests that rely on a server running
 """
 import pytest
+from thrift.transport import TSocket, TTransport
+from thrift.transport.TSocket import TTransportException
+
 from pymapd import connect
+
+socket = TSocket.TSocket("localhost", 9091)
+transport = TTransport.TBufferedTransport(socket)
+
+try:
+    transport.open()
+except TTransportException:
+    pytestmark = pytest.mark.skip("mapd not running on localhost:9091. "
+                                  "Skipping integration tests")
 
 
 @pytest.mark.parametrize('protocol', ['http', 'binary'])
