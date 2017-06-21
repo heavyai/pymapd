767aa4656cd8d983e1bd2c5db276c885628435f4
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-21 11:17:47 -0500

    Fix lint error

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires=['six', 'thrift']
+install_requires = ['six', 'thrift']
 
 if sys.version_info[0] == 2:
     install_requires.append("typing")
