009d3c4981e68280aebc535bff80213f31740404
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-08-22 16:28:54 -0500

    ENH: Add load_table_binary
    This adds the option to load data using the `load_table_binary` client
    method by passing `binary=True` to `Connection.load_table`. The early benchmarks
    aren't all that impressive. Loading 100,000 rows into a table of
    (int, float, str) takes
    
    ```python
    %time con.load_table('baz', data, binary=True)
    CPU times: user 1.39 s, sys: 74.9 ms, total: 1.47 s
    Wall time: 2.65 s
    ```
    
    ```python
    %time con.load_table('baz', data, binary=False)
    CPU times: user 1.03 s, sys: 52.8 ms, total: 1.08 s
    Wall time: 2.1 s
    ```
    
    So slightly slower. Some very early profiling indicates that (for this dataset)
    we spend about 75% of the time converting from a list of tuples to a list of
    Thrift structures (see `_build_input_rows_binary`). The remainder is spent
    in a send/recv with the MapD server.

--- pymapd/_loaders.py
+++ pymapd/_loaders.py
@@ -1,7 +1,8 @@
 """
 Internal helpers for loading data
 """
-from mapd.ttypes import TStringRow, TStringValue
+from itertools import cycle
+from mapd.ttypes import TStringRow, TStringValue, TRow, TDatumVal, TDatum
 
 
 def _build_input_rows(data):
@@ -11,3 +12,43 @@ def _build_input_rows(data):
         input_row.cols = [TStringValue(str(x)) for x in row]
         input_data.append(input_row)
     return input_data
+
+
+def _build_input_rows_binary(data, types):
+    try:
+        import pandas as pd
+    except ImportError:
+        pass
+    else:
+        if isinstance(data, pd.DataFrame):
+            data = data.itertuples(index=False)
+
+    types = cycle(types)
+
+    binary_rows = []
+    for row in data:
+        trow = []
+        for col, t in zip(row, types):
+            if t == 'INT':
+                trow.append(TDatum(TDatumVal(int_val=col)))
+            elif t == 'FLOAT':
+                trow.append(TDatum(TDatumVal(real_val=col)))
+            elif t == 'STR':
+                trow.append(TDatum(TDatumVal(str_val=col)))
+        binary_rows.append(TRow(trow))
+    return binary_rows
+
+
+def _infer_type(column):
+    from pandas.api.types import (is_any_int_dtype,
+                                  is_float_dtype,
+                                  is_string_dtype)
+
+    if is_any_int_dtype(column):
+        return 'int'
+    elif is_float_dtype(column):
+        return 'float'
+    elif is_string_dtype(column):
+        return 'string'
+    else:
+        raise TypeError("Unsupported type {}".format(type(column)))
