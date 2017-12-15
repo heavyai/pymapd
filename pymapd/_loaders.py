c38461f063af1b63c8ab7b99e96dbb94af3fed76
Keith Kraus <keith.j.kraus@gmail.com>
2017-12-15 12:29:59 -0500

    handle lists in building input rows

--- pymapd/_loaders.py
+++ pymapd/_loaders.py
@@ -8,6 +8,6 @@ def _build_input_rows(data):
     input_data = []
     for row in data:
         input_row = TStringRow()
-        input_row.cols = [TStringValue(str(x)) for x in row]
+        input_row.cols = ["{"+",".join(str(y) for y in x)+"}" if type(x)==list else TStringValue(str(x)) for x in row]
         input_data.append(input_row)
     return input_data
