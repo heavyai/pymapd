5831ecfcd7d580111efb83dfa76a2d4f7190b104
Keith Kraus <keith.j.kraus@gmail.com>
2017-12-15 13:12:59 -0500

    pep8 styling fix

--- pymapd/_loaders.py
+++ pymapd/_loaders.py
@@ -3,11 +3,15 @@ Internal helpers for loading data
 """
 from mapd.ttypes import TStringRow, TStringValue
 
-
 def _build_input_rows(data):
     input_data = []
     for row in data:
         input_row = TStringRow()
-        input_row.cols = ["{"+",".join(str(y) for y in x)+"}" if type(x)==list else TStringValue(str(x)) for x in row]
+        input_row.cols = [
+            "{"+",".join(str(y) for y in x)+"}"
+            if type(x) in (list, tuple)
+            else TStringValue(str(x)) for x in row
+        ]
         input_data.append(input_row)
     return input_data
+
