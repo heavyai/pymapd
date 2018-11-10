7213084591768c9ed7f1900a635ae80079c6455d
JP <jpharvey@mapd.com>
2018-11-09 17:57:44 -0800

    Fix linting issue

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -84,7 +84,8 @@ class TestLoaders(object):
             # unreliable since if there is no number outside the int32
             # bounds in a column with nulls then we will be assuming int
             "int_": np.array([0, 1, None], dtype=np.object),
-            "bigint_": np.array([0, 9223372036854775807, None], dtype=np.object),
+            "bigint_": np.array([0, 9223372036854775807, None],
+                                dtype=np.object),
             "double_": np.array([0, 1, None], dtype=np.float64),
             "varchar_": ["a", "b", None],
             "text_": ['a', 'b', None],
