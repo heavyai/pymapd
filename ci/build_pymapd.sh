05e69cee40ea15017cab5c9d47e9b27468854d99
randyzwitch <rzwitch@gmail.com>
2018-11-15 13:12:54 -0500

    Try again for conda-verify

--- ci/build_pymapd.sh
+++ ci/build_pymapd.sh
@@ -1,4 +1,4 @@
 set -e
 
 echo "Building pymapd"
-conda build conda-recipes/pymapd -c conda-forge -c numba -c gpuopenanalytics/label/dev -c defaults --python ${PYTHON}
+conda build conda-recipes/pymapd -c conda-forge -c numba -c gpuopenanalytics/label/dev -c defaults --python ${PYTHON} -c anaconda conda-verify
