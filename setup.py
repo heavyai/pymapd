98c80518c3e4c53a3040afe4fd7fe51a9ab9882e
Randy Zwitch <rzwitch@gmail.com>
2019-01-07 10:51:07 -0500

    Update pytest
    newest pytest-cov drops support for pytest <3.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = ['six', 'thrift == 0.11.0', 'sqlalchemy', 'numpy', 'pandas',
 
 
 doc_requires = ['sphinx', 'numpydoc', 'sphinx-rtd-theme']
-test_requires = ['coverage', 'pytest == 3.3.1', 'pytest-mock']
+test_requires = ['coverage', 'pytest == 3.6', 'pytest-mock']
 dev_requires = doc_requires + test_requires
 gpu_requires = ['cudf', 'libcudf']
 complete_requires = dev_requires + gpu_requires
