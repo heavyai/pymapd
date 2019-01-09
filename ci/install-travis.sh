b967ebbae6dfcc384c4dae0435ba056a9af083e7
randyzwitch <rzwitch@gmail.com>
2019-01-09 14:31:32 -0500

    Try again to make python version stick

--- ci/install-travis.sh
+++ ci/install-travis.sh
@@ -6,6 +6,7 @@ echo "[install-travis]"
 MINICONDA_DIR="$HOME/miniconda3"
 time wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
 time bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
+export PATH="$MINICONDA_DIR/bin:$PATH"
 
 echo
 echo "[show conda]"
@@ -24,17 +25,8 @@ echo
 echo "[add channels]"
 conda config --add channels conda-forge || exit 1
 
-conda env create -f environment.yml python=${PYTHON}
+conda env create -q -f environment.yml python=${PYTHON}
 source activate omnisci-dev
-
-#list of dev packages not needed for general conda environment.yml file
-conda install -q \
-      coverage \
-      flake8 \
-      "pytest>=3.6,<4.0" \
-      pytest-cov \
-      pytest-mock \
-      mock
 
 pip install -e .
 conda list omnisci-dev
