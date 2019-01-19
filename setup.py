d17ea8d90a9675aa47f02a1ef65042cf4d0576eb
Hugo <hugovk@users.noreply.github.com>
2019-01-19 23:40:22 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
 
     license='Apache Software License',
 
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
