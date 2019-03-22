0413cf1eb0c66c5f300960b3a167b149170b09f7
Randy Zwitch <rzwitch@gmail.com>
2019-03-22 08:43:30 -0400

    Fix array handling (#183)
    Provides ability to return list columns within sql_execute. Closes #68

--- pymapd/_parsers.py
+++ pymapd/_parsers.py
@@ -48,25 +48,56 @@ _thrift_encodings_to_values = T.TEncodingType._NAMES_TO_VALUES
 _thrift_values_to_encodings = T.TEncodingType._VALUES_TO_NAMES
 
 
+def _format_result_timestamp(desc, arr):
+
+    return [None if v is None else
+            datetime_in_precisions(v, desc.col_type.precision)
+            for v in arr]
+
+
+def _format_result_date(arr):
+
+    base = datetime.datetime(1970, 1, 1)
+    return [None if v is None else
+            (base + datetime.timedelta(seconds=v)).date()
+            for v in arr]
+
+
+def _format_result_time(arr):
+
+    return [None if v is None else seconds_to_time(v) for v in arr]
+
+
 def _extract_col_vals(desc, val):
     # type: (T.TColumnType, T.TColumn) -> Any
+
     typename = T.TDatumType._VALUES_TO_NAMES[desc.col_type.type]
     nulls = val.nulls
 
-    vals = getattr(val.data, _typeattr[typename] + '_col')
-    vals = [None if null else v
-            for null, v in zip(nulls, vals)]
+    # arr_col has multiple levels to parse, not accounted for in original code
+    # https://github.com/omnisci/pymapd/issues/68
+    if hasattr(val.data, 'arr_col') and val.data.arr_col:
+        vals = [None if null else getattr(v.data, _typeattr[typename] + '_col')
+                for null, v in zip(nulls, val.data.arr_col)]
 
-    if typename == 'TIMESTAMP':
-        vals = [None if v is None else
-                datetime_in_precisions(v, desc.col_type.precision)
-                for v in vals]
-    elif typename == 'DATE':
-        base = datetime.datetime(1970, 1, 1)
-        vals = [None if v is None else
-                (base + datetime.timedelta(seconds=v)).date() for v in vals]
-    elif typename == 'TIME':
-        vals = [None if v is None else seconds_to_time(v) for v in vals]
+        if typename == 'TIMESTAMP':
+            vals = [_format_result_timestamp(desc, v) for v in vals]
+        elif typename == 'DATE':
+            vals = [_format_result_date(v) for v in vals]
+        elif typename == 'TIME':
+            vals = [_format_result_time(v) for v in vals]
+
+    # else clause original code path
+    else:
+        vals = getattr(val.data, _typeattr[typename] + '_col')
+        vals = [None if null else v for null, v in zip(nulls, vals)]
+
+        if typename == 'TIMESTAMP':
+            vals = _format_result_timestamp(desc, vals)
+        elif typename == 'DATE':
+            vals = _format_result_date(vals)
+        elif typename == 'TIME':
+            vals = _format_result_time(vals)
 
     return vals
 
