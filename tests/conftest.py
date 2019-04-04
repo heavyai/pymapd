545cf7142d73c946b2a9d1fa2adf92c8a7b70428
randyzwitch <rzwitch@gmail.com>
2019-04-03 21:00:44 -0400

    Move date_table to where its used
    To make it more obvious, move date_table into script where used

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,31 +53,6 @@ def con(mapd_server):
                    port=6274, protocol='binary', dbname='mapd')
 
 
-@pytest.fixture(scope="session")
-def date_table(con):
-    """A sample with date and time columns
-
-    - date_ : DATE
-    - datetime_ : TIMESTAMP
-    - time_ : TIME
-    """
-    name = 'dates'
-    drop = 'drop table if exists {};'.format(name)
-    c = con.cursor()
-    c.execute(drop)
-    create = ('create table {} (date_ DATE, datetime_ TIMESTAMP, '
-              'time_ TIME);'.format(name))
-    c.execute(create)
-    i1 = ("INSERT INTO {} VALUES ('2006-01-05','2006-01-01T12:00:00',"
-          "'12:00');".format(name))
-    i2 = ("INSERT INTO {} VALUES ('1901-12-14','1901-12-13T20:45:53',"
-          "'23:59:00');".format(name))
-    c.execute(i1)
-    c.execute(i2)
-    yield name
-    c.execute(drop)
-
-
 @pytest.fixture
 def all_types_table(con):
     """A table with all the supported types
