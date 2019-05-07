c1672a9a6941536e93a2a8d0b10cadca196cafc0
Randy Zwitch <rzwitch@gmail.com>
2019-05-07 14:06:09 -0700

    Remove generate_bindings.sh
    This script out of date, as recursive fix no longer needed.

--- scripts/generate_bindings.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Generating bindings to ${1-.}"
-thrift -gen py -out ${1-.} ${MAPD_HOME}/mapd.thrift
-mv ${1-.}/mapd/ttypes.py ${1-.}/mapd/ttypes-backup.py
-echo "Fixing recursive structs"
-python ${MAPD_HOME}/SampleCode/fix_recursive_structs.py ${1-.}/mapd/ttypes-backup.py ${1-.}/mapd/ttypes.py
-rm ${1-.}/__init__.py
