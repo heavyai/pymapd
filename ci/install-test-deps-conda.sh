8cc14c119e4ab21cfa41df55b990a66d1132c521
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-05-08 21:26:26 -0400

    Update pinnings

--- ci/install-test-deps-conda.sh
+++ ci/install-test-deps-conda.sh
@@ -10,15 +10,15 @@ conda config --add channels conda-forge
 conda config --add channels rapidsai
 conda config --add channels nvidia
 
-conda create -n omnisci-dev python=${PYTHON} \
-'thrift=0.13.0' \
-'cudf=0.13' \
-'cudatoolkit=10.1' \
-'arrow-cpp=0.15.0' \
-'pyarrow==0.15.0' \
-'pandas>=0.25,<0.26' \
-sqlalchemy \
-numpy \
+conda create --yes -n omnisci-dev python=${PYTHON:-3.7} \
+thrift=0.13.0 \
+cudf=0.13 \
+cudatoolkit=10.1 \
+arrow-cpp=0.15.0 \
+pyarrow==0.15.0 \
+pandas>=1.0 \
+sqlalchemy>=1.3 \
+numpy>=1.16 \
 numpydoc \
 coverage \
 flake8 \
@@ -29,7 +29,7 @@ shapely \
 sphinx  \
 requests \
 sphinx_rtd_theme \
-'rbc==0.2.2'
+rbc=0.2.2
 
 conda activate omnisci-dev
 
