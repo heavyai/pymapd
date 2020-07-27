f7eb349eef451402f1441e3e2fb96c2257a731f9
Joel Clay <joel.clay@omnisci.com>
2020-07-27 17:07:35 -0400

    Increase specificity of regex

--- ci/install-test-deps-conda.sh
+++ ci/install-test-deps-conda.sh
@@ -7,11 +7,11 @@ conda install -q conda-build anaconda-client conda-verify --yes
 
 # create a copy of the environment file, replacing
 # with the python version we specify.
-sed -E "s/python.+$/python=$PYTHON/" ./environment_gpu.yml > /tmp/environment_gpu_${PYTHON}.yml
+sed -E "s/- python[^[:alpha:]]+$/- python=$PYTHON/" ./environment_gpu.yml > /tmp/environment_gpu_${PYTHON}.yml
 
 conda env create -f /tmp/environment_gpu_${PYTHON}.yml
 
-conda activate omnisci-dev
+conda activate omnisci-gpu-dev
 
 pip install -e .
 conda list omnisci-dev
