3aecb26b05c7ac996b478935d54aea5f2e5cb6f0
jp-harvey <jpharvey@cloudquarterback.com>
2020-12-15 17:57:59 -0800

    Additional pyomniscidb private modules

--- pymapd/__init__.py
+++ pymapd/__init__.py
@@ -1,4 +1,18 @@
-import pyomnisci
-import sys
+from pyomnisci import *
 
-sys.modules['pymapd'] = pyomnisci
+
+def _deprication_warning():
+    """
+    Use a function here so that `os` does not get imported as a
+    property of `pymapd`
+    """
+    import os
+    disablewarning = os.environ.get('DISABLE_PYMAPD_WARNING', False)
+    if not disablewarning:
+        print('WARNING: pymapd is now pyomnisci, please update libraries',
+              'and references as pymapd may cease to function as expected',
+              'in the future. To disable this warning, set the environment',
+              'variable DISABLE_PYMAPD_WARNING to a truthy value.')
+
+
+_deprication_warning()
---
+++ pymapd/_samlutils.py
@@ -0,0 +1,1 @@
+from omnisci._samlutils import *
